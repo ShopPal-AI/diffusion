bb699fde105d978955589909a3f8d053496fca06
zhuguangbin <zhuguangbin@shoppal.ai>
2024-01-03 18:40:56 +0800

    add convert_coco script

---
+++ scripts/convert_coco.sh
@@ -0,0 +1,4 @@
+python3 convert_coco.py \
+	--remote /data0/datasets/sd/coco/mds_train \
+	--data_path /data0/datasets/sd/coco/train2017 \
+	--captions_path /data0/datasets/sd/coco/annotations/captions_train2017.json
